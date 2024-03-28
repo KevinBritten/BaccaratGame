cec80d18294aaa1f9761c094c4add4cbb384f061
Kevin Britten <kevinjbritten@gmail.com>
2024-03-28 16:27:00 -0400

    create initial implimentation of Player class

--- BaccaratGame/Player.cs
+++ BaccaratGame/Player.cs
@@ -8,5 +8,44 @@ namespace BaccaratGame
 {
     internal class Player
     {
+        private String _name;
+        private decimal _funds;
+        //TODO: add avatar files
+        //private Image _avatar;
+        private decimal _currentBet;
+
+        public Player(String name, decimal funds, String avatarFilePath)
+        {
+            _name = name;
+            _funds = funds;
+            //_avatar = Image.FromFile(avatarFilePath);
+        }
+
+        public String Name
+        {
+            get { return _name; }
+        }
+
+        public decimal Funds
+        {
+            get { return _funds; }
+            set { _funds = value; }
+        }
+
+        //public Image Avatar
+        //{
+        //    get { return _avatar; }
+        //}
+
+        public decimal CurrentBet
+        {
+            get { return _currentBet; }
+            set { if (value <= _funds) _currentBet = value;
+                else _currentBet = _funds;
+            }
+        }
+
+        public void clearBet() { _currentBet = 0; }
+       
     }
 }
