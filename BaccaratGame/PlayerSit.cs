f52291ce8fc2f9ff66c50c7740d909cf634a4c2a
Kevin Britten <kevinjbritten@gmail.com>
2024-04-11 16:25:52 -0400

    remove unused function MakeGrayScaleImage

--- BaccaratGame/PlayerSit.cs
+++ BaccaratGame/PlayerSit.cs
@@ -86,37 +86,6 @@ namespace BaccaratGame
                 avatarPictureBox.Image = grayScaleImage;
             }
         }
-       
-        public static Bitmap MakeGrayscale(Image original)
-        {
-            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
-            using (Graphics g = Graphics.FromImage(newBitmap))
-            {
-                ColorMatrix colorMatrix = new ColorMatrix(
-                    new float[][]
-                    {
-                new float[] {.3f, .3f, .3f, 0, 0},
-                new float[] {.59f, .59f, .59f, 0, 0},
-                new float[] {.11f, .11f, .11f, 0, 0},
-                new float[] {0, 0, 0, 1, 0},
-                new float[] {0, 0, 0, 0, 1}
-                    });
-
-                // Create image attributes
-                using (ImageAttributes attributes = new ImageAttributes())
-                {
-                    // Set the color matrix attribute
-                    attributes.SetColorMatrix(colorMatrix);
-
-                    // Draw the original image on the new image
-                    // using the grayscale color matrix
-                    g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
-                        0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
-                }
-            }
-            return newBitmap;
-        }
-
 
         private void CancelButton_Click(object sender, EventArgs e)
         {
